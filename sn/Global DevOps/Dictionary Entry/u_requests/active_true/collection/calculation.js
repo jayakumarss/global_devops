013f3a6055729abbfada55af1c233d14263fd58d
System Administrator <admin@example.com>
2019-10-29 11:35:49 -0400

    Changes by 'System Administrator' on 'Global DevOps - 1.0.0' -- Build #1

---
+++ sn/Global DevOps/Dictionary Entry/u_requests/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Global DevOps
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-29 15:33:57
+ * Created By  : admin
+ * Updated On  : 2019-10-29 15:33:57
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=38d1992ddb700010343f58b3ca9619df
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
---
+++ sn/Global DevOps/Dictionary Entry/u_requests/active_true/u_caller/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Global DevOps
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-29 15:33:56
+ * Created By  : admin
+ * Updated On  : 2019-10-29 15:33:56
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=2281dd29db700010343f58b3ca9619fd
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
